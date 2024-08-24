70c61b9fdaad4dde1fcce8eff46f715019b4b707
Yiding He <yiding.he@gmail.com>
2024-08-24 16:17:40 +0800

    init

---
+++ src/main/java/com/hyd/hamis/HydrogenAmisApplication.java
@@ -0,0 +1,11 @@
+package com.hyd.hamis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydrogenAmisApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(HydrogenAmisApplication.class, args);
+  }
+}
---
+++ src/test/java/com/hyd/hamis/HydrogenAmisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyd.hamis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HydrogenAmisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
